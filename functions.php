44877d0581614753e546dd6dfeec641cd2a5d70e
tiennguyenvan <nguyentien.jobs@gmail.com>
2023-07-16 11:17:18 +0700

    Add plugin versions

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 add_filter('sneeit_required_plugins', function($plugins) {
-    $plugins[] = '*dragblock'; // this is required
-    $plugins[] = '*sneeit-core'; // this is required
+    $plugins[] = '*dragblock_0.0.1'; // this is required
+    $plugins[] = '*sneeit-core_1.0'; // this is required
     // $plugins[] = '*breadcrumb-navxt_wp6.0.0'; // this is required
     // $plugins[] = 'envato-market_2.0.1';
     // $plugins[] = 'classic-editor';
