8b9aa84c88fbc7b77312a122c1ce80395274e5b6
tiennguyenvan <nguyentien.jobs@gmail.com>
2023-08-11 18:46:31 +0700

    New TGMA Features

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 add_filter('sneeit_required_plugins', function($plugins) {
     $plugins[] = '*dragblock_0.0.1'; // this is required
-    $plugins[] = '*sneeit-core_1.0'; // this is required
+    $plugins[] = '*sneeit-core_1.0'; // this is required    
     // $plugins[] = '*breadcrumb-navxt_wp6.0.0'; // this is required
     // $plugins[] = 'envato-market_2.0.1';
     // $plugins[] = 'classic-editor';
     return $plugins;
 });
-
 if (is_admin() && !wp_doing_ajax()) {
 	require_once 'plugins/plugins.php';
 }
