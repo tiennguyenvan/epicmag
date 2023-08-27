cd65a684aeca013af1cb89f985b63a37ace9b598
tiennguyenvan <nguyentien.jobs@gmail.com>
2023-08-27 12:21:43 +0700

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 
-const { updateVersion, cleanFolder, copyToRelease, copyToDEV, zipFolder, copyFromTo } = require('C:/xampp/htdocs/sneeit/wp-content/themes/sneeit/gulpfile-lib.js');
+const { updateVersion, cleanFolder, copyToRelease, copyToDEV, zipFolder, copyFromTo, getVersion } = require('C:/xampp/htdocs/sneeit/wp-content/themes/sneeit/gulpfile-lib.js');
 
 const slug = 'epicmag';
 const basePath = `f:/Dropbox/sneeit.com/wordpress-themes/${slug}`;
@@ -9,13 +9,16 @@ const versionFile = 'style.css';
 const versionStatus = false;
 
 exports.updateVersion = function (cb) { updateVersion(versionFile, versionStatus); cb(); }
-
-// exports.cleanRelease = function (cb) { cleanFolder(`${releasePath}/*`); cb(); }
 exports.copyToRelease = function (cb) { copyToRelease(releasePath); cb(); }
-exports.zipRelease = function (cb) { zipFolder(releasePath); cb(); }
-
-// exports.cleanDEV = function (cb) { cleanFolder([`${DEVPath}/**/*`, `${DEVPath}/*`, `${DEVPath}/**`]); cb(); }
 exports.copyToDEV = function (cb) { copyToDEV(DEVPath); cb(); }
+exports.zipRelease = function (cb) {
+    const version = getVersion(versionFile);
+    zipFolder(releasePath, `${slug}-${version}.zip`); cb();
+}
+exports.zipDEV = function (cb) {
+    const version = getVersion(versionFile);
+    zipFolder(DEVPath, `${slug}-${version}.zip`); cb();
+}
 
 exports.cleanDemos = function (cb) { cleanFolder(`${basePath}/release/demos/*`); cb(); }
 exports.copyDemos = function (cb) { copyFromTo(`demos/**`, `${basePath}/release/demos`); cb(); }
