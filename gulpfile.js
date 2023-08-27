869a3e1f8f2c8ec950c49d7744fa20834bd248a6
tiennguyenvan <nguyentien.jobs@gmail.com>
2023-08-27 08:48:30 +0700

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,21 @@
-const gulp = require('gulp');
-const replace = require('gulp-replace');
-const zip = require('gulp-zip');
-const clean = require('gulp-clean');
 
-/**
- * Please configure your own paths here
- */
-const basePath = 'f:/Dropbox/sneeit.com/wordpress-themes/epicmag';
-const releasePath = basePath + '/release';
-const devPath = basePath + '/dev';
-const itemSlug = 'epicmag';
+const { updateVersion, cleanFolder, copyToRelease, copyToDEV, zipFolder, copyFromTo } = require('C:/xampp/htdocs/sneeit/wp-content/themes/sneeit/gulpfile-lib.js');
 
-const releaseExcludes = ['**/*', '!demos/**', '!src/**', '!**/.vscode', '!node_modules/**', '!.git/**', '!.gitattributes', '!.gitignore', '!gulpfile.js', '!package.json', '!package-lock.json', '!webpackage.config.js']
-const devExcludes = ['**/*', '**/.vscode/**', '**/.gitignore', '**/.gitattributes', '!git/**', '!build/**', '!node_modules/**'];
+const slug = 'epicmag';
+const basePath = `f:/Dropbox/sneeit.com/wordpress-themes/${slug}`;
+const releasePath = `${basePath}/release/${slug}`;
+const DEVPath = `${basePath}/DEV/${slug}`;
+const versionFile = 'style.css';
+const versionStatus = false;
 
-function updateStyleVersion() {
-    const currentDate = new Date();
-    const year = String(currentDate.getFullYear()).slice(-2); // Get last two digits of the year
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    const newVersion = `${year}.${month}${day}.${hours}${minutes}`;
+exports.updateVersion = function (cb) { updateVersion(versionFile, versionStatus); cb(); }
 
-    const versionPattern = /Version: .*/g;
+// exports.cleanRelease = function (cb) { cleanFolder(`${releasePath}/*`); cb(); }
+exports.copyToRelease = function (cb) { copyToRelease(releasePath); cb(); }
+exports.zipRelease = function (cb) { zipFolder(releasePath); cb(); }
 
-    return gulp
-        .src('style.css') // Adjust the source path as per your project's structure
-        .pipe(replace(versionPattern, `Version: ${newVersion}`))
-        .pipe(gulp.dest('./')); // Outputs to the same folder as gulpfile.js
-}
+// exports.cleanDEV = function (cb) { cleanFolder([`${DEVPath}/**/*`, `${DEVPath}/*`, `${DEVPath}/**`]); cb(); }
+exports.copyToDEV = function (cb) { copyToDEV(DEVPath); cb(); }
 
-
-
-// Copy task to copy files to the release folder
-function copyToRelease() {
-    // Clean the destination folder before copying files
-    return gulp.src(releasePath + '/' + itemSlug, { read: false, allowEmpty: true })
-        .pipe(clean())
-        // then copy but exclude some files and folders        
-        .on('end', function () {
-            return gulp
-                .src(releaseExcludes)
-                .pipe(gulp.dest(releasePath + '/' + itemSlug));
-        });
-}
-
-function copyToDevelopment() {
-    // Clean the destination folder before copying files
-    return gulp.src(devPath + '/' + itemSlug, { read: false, allowEmpty: true })
-        .pipe(clean())
-        // then copy but exclude some files and folders        
-        .on('end', function () {
-            return gulp
-                .src(devExcludes)
-                .pipe(gulp.dest(devPath + '/' + itemSlug));
-        });
-}
-
-/**
- * We have to separate the create zip here to make it work 
- * If we push into the series, the timing is not enough to run the createzip function
- * @returns 
- */
-function createReleaseZip() {
-    return gulp.src(releasePath + '/' + itemSlug + '/**/*')
-        .pipe(zip(itemSlug + '.zip'))
-        .pipe(gulp.dest(releasePath));
-}
-
-exports.createReleaseZip = createReleaseZip;
-
-
-function releaseDemos() {
-    // Clean the destination folder before copying files
-    return gulp.src(releasePath + '/demos/**/*', { read: false, allowEmpty: true })
-        .pipe(clean())
-        // then copy but exclude some files and folders        
-        .on('end', function () {
-            return gulp
-                .src(['**/demos/**/*'])
-                .pipe(gulp.dest(releasePath));
-        });
-}
-exports.releaseDemos = releaseDemos;
-
-gulp.task('build', gulp.series(updateStyleVersion, copyToRelease, copyToDevelopment));
-gulp.task('start', gulp.series(updateStyleVersion));
-
-
+exports.cleanDemos = function (cb) { cleanFolder(`${basePath}/release/demos/*`); cb(); }
+exports.copyDemos = function (cb) { copyFromTo(`demos/**`, `${basePath}/release/demos`); cb(); }
